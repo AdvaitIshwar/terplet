47a79b1685df528679e99a66c7538fea0232b2e3
Derek Yu <derek.c.yu@gmail.com>
2020-07-28 22:18:11 -0400

    added user authentication

--- terplet/src/App.js
+++ terplet/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
+import { db, auth } from "./firebase";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Fade from "@material-ui/core/Fade";
+import TextField from '@material-ui/core/TextField';
 
 function getModalStyle() {
   const top = 50;
@@ -33,7 +34,29 @@ function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser)
+
+      } else {
+        setUser(null);
+      }
+    })
+
+    return () => {
+      unsubscribe();
+    }
+
+  }, [user, username]);
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -46,12 +69,103 @@ function App() {
     });
   }, []);
 
+  const signUp = (event) => {
+    event.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => alert(error.message));
+    
+    setOpen(false);
+  }
+
+  const signIn = (event) => {
+    event.preventDefault();
+    auth.signInWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message));
+
+    setOpenSignIn(false);
+  }
+
   return (
     <div className="App">
       <Modal open={open} onClose={() => setOpen(false)}>
         <Fade in={open}>
           <div style={modalStyle} className={classes.paper}>
-            Sign Up
+            <center><h3>TERPLET</h3></center>
+            <br />
+            <form className="app__signup">
+              <TextField
+                required
+                id="outlined-username-input"
+                label="Username"
+                type="username"
+                variant="outlined"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <br />
+              <TextField
+                required
+                id="outlined-email-input"
+                label="Email"
+                type="email"
+                autoComplete="current-email"
+                variant="outlined"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <br />
+                <TextField
+                required
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="outlined"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br />
+              <Button type="submit" onClick={signUp}>Sign Up</Button>
+            </form>
+          </div>
+        </Fade>
+      </Modal>
+
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <Fade in={openSignIn}>
+          <div style={modalStyle} className={classes.paper}>
+            <center><h3>TERPLET</h3></center>
+              <br />
+              <form className="app__signup">
+                <TextField
+                  required
+                  id="outlined-email-input"
+                  label="Email"
+                  type="email"
+                  autoComplete="current-email"
+                  variant="outlined"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
+                  <TextField
+                  required
+                  id="outlined-password-input"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  variant="outlined"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <Button type="submit" onClick={signIn}>Sign In</Button>
+            </form>
           </div>
         </Fade>
       </Modal>
@@ -64,7 +178,13 @@ function App() {
         ></img>
       </div>
 
+      {user ? (<Button onClick={() => auth.signOut()}>Logout</Button>):
+      (
+      <div>
+      <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
       <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      </div>
+      )}
 
       {posts.map(({ id, post }) => (
         <Post
